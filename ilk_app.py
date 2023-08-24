59fa85d94485b58d0f64e7c1d21508386e79f856
Samet Erten <52792863+samieltr@users.noreply.github.com>
2023-08-25 02:21:55 +0300

    Python ilk_app.py
    Müşteri bilgileri yazdırma ve sipariş tutar hesaplama

---
+++ ilk_app.py
@@ -0,0 +1,27 @@
+# Bir müşteri için aşağıdaki bilgileri doldurunuz
+musteriAd = "Samet" 
+musteriSoyad = "Erten"
+musteriAd_Soyad = musteriAd + " " + musteriSoyad
+musteriCinsiyet = True #Erkek - False Kadın
+musteriTc = "11111111111"
+musteriDogumYil = 1999
+musteriAdres ="Ankara Aydınlıkevler"
+musteriYas = 2023 - musteriDogumYil
+
+print(musteriAd)
+print(musteriSoyad)
+print(musteriAd_Soyad)
+print(musteriCinsiyet)
+print(musteriTc)
+print(musteriDogumYil)
+print(musteriAdres)
+print(musteriYas)
+
+#Aşağıdaki girilmiş siparişlerin toplam bilgisini hesaplayın
+
+siparis1 = 110
+siparis2 = 1100.5
+siparis3 = 356.95
+toplam = siparis1 + siparis2 + siparis3
+
+print("Toplam =",toplam,"₺")
